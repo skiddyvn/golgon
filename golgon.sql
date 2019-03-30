d68eca7ab20ccef8cfc63bd2b0112534b7f335ee
skiddyvn <45237572+skiddyvn@users.noreply.github.com>
2019-03-30 17:54:30 +0700

    Add files via upload

---
+++ golgon.sql
@@ -0,0 +1,191 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th3 30, 2019 lúc 11:53 AM
+-- Phiên bản máy phục vụ: 10.1.37-MariaDB
+-- Phiên bản PHP: 7.3.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `golgon`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `comments`
+--
+
+CREATE TABLE `comments` (
+  `cid` int(11) NOT NULL,
+  `ccomment` text NOT NULL,
+  `ctime` datetime NOT NULL,
+  `cuser` int(11) NOT NULL,
+  `cproduct` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `orders`
+--
+
+CREATE TABLE `orders` (
+  `oid` int(11) NOT NULL,
+  `ouser` varchar(45) NOT NULL,
+  `oaddress` varchar(256) NOT NULL,
+  `ophone` varchar(15) NOT NULL,
+  `ototalprice` int(11) NOT NULL,
+  `ototalquantity` int(11) NOT NULL,
+  `otime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `orders_products`
+--
+
+CREATE TABLE `orders_products` (
+  `oid` int(11) NOT NULL,
+  `pid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `products`
+--
+
+CREATE TABLE `products` (
+  `pid` int(11) NOT NULL,
+  `pname` varchar(512) NOT NULL,
+  `pprice` int(11) NOT NULL,
+  `pquantity` int(11) NOT NULL,
+  `pbrand` varchar(45) NOT NULL,
+  `pcategory` varchar(45) NOT NULL,
+  `pdescription` text NOT NULL,
+  `pimage` varchar(1024) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `users`
+--
+
+CREATE TABLE `users` (
+  `uid` int(11) NOT NULL,
+  `ufirstname` varchar(100) NOT NULL,
+  `ulastname` varchar(100) NOT NULL,
+  `uusername` varchar(45) NOT NULL,
+  `upassword` varchar(45) NOT NULL,
+  `uavatar` varchar(1024) NOT NULL,
+  `umail` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`cid`),
+  ADD KEY `fk_comments_users` (`cuser`),
+  ADD KEY `fk_comments_products` (`cproduct`);
+
+--
+-- Chỉ mục cho bảng `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`oid`);
+
+--
+-- Chỉ mục cho bảng `orders_products`
+--
+ALTER TABLE `orders_products`
+  ADD PRIMARY KEY (`oid`,`pid`),
+  ADD KEY `fk_ou_products` (`pid`);
+
+--
+-- Chỉ mục cho bảng `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Chỉ mục cho bảng `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `oid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `products`
+--
+ALTER TABLE `products`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `users`
+--
+ALTER TABLE `users`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `fk_comments_products` FOREIGN KEY (`cproduct`) REFERENCES `products` (`pid`),
+  ADD CONSTRAINT `fk_comments_users` FOREIGN KEY (`cuser`) REFERENCES `users` (`uid`);
+
+--
+-- Các ràng buộc cho bảng `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `fk_orders_users` FOREIGN KEY (`oid`) REFERENCES `users` (`uid`);
+
+--
+-- Các ràng buộc cho bảng `orders_products`
+--
+ALTER TABLE `orders_products`
+  ADD CONSTRAINT `fk_ou_orders` FOREIGN KEY (`oid`) REFERENCES `orders` (`oid`),
+  ADD CONSTRAINT `fk_ou_products` FOREIGN KEY (`pid`) REFERENCES `products` (`pid`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
