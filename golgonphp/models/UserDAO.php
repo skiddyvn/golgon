c3c2a22eb87bf0323c9bf99cbcb19da914341026
skiddyvn <45237572+skiddyvn@users.noreply.github.com>
2019-04-10 08:09:17 +0700

    Add files via upload

--- golgonphp/models/UserDAO.php
+++ golgonphp/models/UserDAO.php
@@ -8,14 +8,51 @@ class UserDAO {
         $stmt->bind_param("ss",$username,$password);
         $stmt->execute();
         $result = $stmt->get_result();
-        if ($result) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    public static function checkRegister() {
+        $data = $result->fetch_assoc();
         
+        $stmt->close();
+        $mysqli->close();
+        
+        if (!$data) {
+            return FALSE;
+        }
+        return $data;
+    }
+    public static function checkRegister($username) {
+        $mysqli = Database::getConnection();
+        $query = "SELECT * FROM users where uusername = ?";
+        $stmt = $mysqli->prepare($query);
+        $stmt->bind_param("s",$username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $data = $result->fetch_assoc();
+        
+        $stmt->close();
+        $mysqli->close();
+        
+        if (!$data) {
+            return TRUE;
+        }
+        return FALSE;
+    }
+    public static function register($user) {
+        $mysqli = Database::getConnection();
+        $query = "INSERT INTO users values(?,?,?,?,?,?,?,?)";
+        $stmt = $mysqli->prepare($query);
+        
+        $userid = $user->getUserid();
+        $firstname = $user->getFirstname();
+        $lastname = $user->getLastname();
+        $username = $user->getUsername();
+        $password = $user->getPassword();
+        $mail = $user->getMail();
+        $avatar = $user->getAvatar();
+        $role = $user->getRole();
+        
+        $stmt->bind_param("isssssss",$userid,$firstname,$lastname,$username,$password,$mail,$avatar,$role);
+        $stmt->execute();
+        $stmt->close();
+        $mysqli->close();
     }
     public static function addUser() {
         
