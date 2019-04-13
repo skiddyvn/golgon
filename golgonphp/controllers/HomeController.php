a730a62a7fb891c19cb9205ffd0a8e7b47d42f39
skiddyvn <45237572+skiddyvn@users.noreply.github.com>
2019-04-13 16:26:17 +0700

    Update HomeController.php

--- golgonphp/controllers/HomeController.php
+++ golgonphp/controllers/HomeController.php
@@ -7,6 +7,26 @@ class HomeController {
         $listProducts = ProductDAO::getListAllProducts();
         $listNewProducts = ProductDAO::getListNewProducts(5);
         $listComponents = ProductDAO::getListProductByComponents();
+        
+        $listTab1 = ProductDAO::getListProductByCategory("desktop",10);
+        $listTab2 = ProductDAO::getListProductByCategory("laptop",10);
+        
+        $listTab3 = ProductDAO::getListProductByCategory("mainboard",10);
+        $listTab4 = ProductDAO::getListProductByCategory("vga",10);
+        $listTab5 = ProductDAO::getListProductByCategory("cpu",10);
+        $listTab6 = ProductDAO::getListProductByCategory("ram",10);
+        $listTab7 = ProductDAO::getListProductByCategory("ssd",10);
+        $listTab8 = ProductDAO::getListProductByCategory("hdd",10);
+        
+        $listTab9 = ProductDAO::getListProductByCategory("monitor",10);
+        $listTab10 = ProductDAO::getListProductByCategory("keyboard",10);
+        $listTab11 = ProductDAO::getListProductByCategory("mouse",10);
+        $listTab12 = ProductDAO::getListProductByCategory("headphone",10);
+        
+        $listTab13 = ProductDAO::getListProductByCategory("cooling",10);
+        $listTab14 = ProductDAO::getListProductByCategory("console",10);
+        $listTab15 = ProductDAO::getListProductByCategory("accessories",10);
+        
         require('views/public/home.php');
     }
     public function contact() {
