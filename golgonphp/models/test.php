244e2f927a100945c69a32ee86c0c7d5333ec769
Sangdk <45009295+Sangdk@users.noreply.github.com>
2019-04-13 21:13:14 +0700

    fix

---
+++ golgonphp/models/test.php
@@ -0,0 +1,11 @@
+<?php
+//require_once 'E:\xampp\htdocs\golgon\golgonphp\config\connection.php';
+//require_once 'E:\xampp\htdocs\golgon\golgonphp\models\ProductDAO.php';
+//
+//$product = ProductDAO::getAProduct(53);
+//$category = $product['pcategory'];
+//$oldimage = $product['pimage'];
+//unlink("E:/xampp/htdocs/golgon/golgonphp/assets/images/products/$category/$oldimage");
+
+
+?>
