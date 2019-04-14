9c7ad5d446acc2cd185fe80c9d0f9ef743547162
skiddyvn <45237572+skiddyvn@users.noreply.github.com>
2019-04-14 13:18:46 +0700

    Update home.js

--- golgonphp/assets/js/home.js
+++ golgonphp/assets/js/home.js
@@ -1,17 +1,65 @@
-;
-(function ($) {
-    'use strict';
-    var $body = $('html, body'), // Define jQuery collection 
-            content = $('body').smoothState({
-        onStart: {
-            duration: 250,
-            render: function () {
-                content.toggleAnimationClass('is-exiting');
-
-                // Scroll user to the top
-                $body.animate({'scrollTop': 0});
-
-            }
+$(document).ready(function () {
+    function translateLeft() {
+        var left = $(this).parent().parent().siblings().find(".abs").css("left");
+        if (parseInt(left) !== 0) {
+            $(this).parent().parent().siblings().find(".abs").css("left", "+=230px");
         }
-    }).data('smoothState');
-})(jQuery);
+    }
+    function translateRight() {
+        var left = $(this).parent().parent().siblings().find(".abs").css("left");
+        if (parseInt(left) !== -1380) {
+            $(this).parent().parent().siblings().find(".abs").css("left", "-=230px");
+        }
+    }
+    $(".prslide-prev").click(translateLeft);
+    $(".prslide-next").click(translateRight);
+    
+    $(".cat-tab").click(function() {
+        var tab = $(this).attr("href").split('#')[1];
+        $(".row-1").not(document.getElementById(tab)).hide();
+        $("#"+tab+"").show();
+        $(".cat-tab").removeClass("selected");
+        $(this).addClass("selected");
+        return false;
+    });
+    $(".cat-tab-2").click(function() {
+        var tab = $(this).attr("href").split('#')[1];
+        $(".row-2").not(document.getElementById(tab)).hide();
+        $("#"+tab+"").show();
+        $(".cat-tab-2").removeClass("selected");
+        $(this).addClass("selected");
+        return false;
+    });
+    $(".cat-tab-3").click(function() {
+        var tab = $(this).attr("href").split('#')[1];
+        $(".row-3").not(document.getElementById(tab)).hide();
+        $("#"+tab+"").show();
+        $(".cat-tab-3").removeClass("selected");
+        $(this).addClass("selected");
+        return false;
+    });
+    $(".cat-tab-4").click(function() {
+        var tab = $(this).attr("href").split('#')[1];
+        $(".row-4").not(document.getElementById(tab)).hide();
+        $("#"+tab+"").show();
+        $(".cat-tab-4").removeClass("selected");
+        $(this).addClass("selected");
+        return false;
+    });
+    $('a[href="#home-news"]').click(function(e) {
+        e.preventDefault();
+        var elementpos = $($(this).attr('href')).offset().top;
+        $('html').animate({scrollTop: elementpos + 'px'}, 400);
+    });
+    window.onscroll = function() {
+            if(document.documentElement.scrollTop > 20 &&  document.documentElement.scrollTop < 4730) {
+                $('#back').show();
+            } else {
+                $('#back').hide();
+            }
+    };
+    $('#back').click(function(e) {
+        e.preventDefault();
+        $('html').animate({scrollTop:0},400);
+    });
+});
