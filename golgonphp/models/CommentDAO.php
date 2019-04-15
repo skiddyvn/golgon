2ddff405e70b82d35835a6816b36b9e22db1236a
skiddyvn <45237572+skiddyvn@users.noreply.github.com>
2019-04-15 10:47:22 +0700

    Update CommentDAO.php

--- golgonphp/models/CommentDAO.php
+++ golgonphp/models/CommentDAO.php
@@ -9,4 +9,18 @@ class CommentDAO {
     public static function updateComment() {
         
     }
+    public static function getListCommentsByProduct($productid) {
+        $mysqli = Database::getConnection();
+        $query = "select * from comments where cproduct = ?";
+        $stmt = $mysqli->prepare($query);
+        $stmt->bind_param("i", $productid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $data = null;
+        while($row = $result->fetch_assoc()){
+            $data[]=$row;
+        }
+        $mysqli->close();
+        return $data;
+    }
 }
