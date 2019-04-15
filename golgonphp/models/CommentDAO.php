e941887863112f2f0889e34761bc2ba0cf3c0103
skiddyvn <45237572+skiddyvn@users.noreply.github.com>
2019-04-15 09:31:19 +0700

    Add files via upload

---
+++ golgonphp/models/CommentDAO.php
@@ -0,0 +1,12 @@
+<?php
+class CommentDAO {
+    public static function addComment() {
+        
+    }
+    public static function deleteComment() {
+        
+    }
+    public static function updateComment() {
+        
+    }
+}
